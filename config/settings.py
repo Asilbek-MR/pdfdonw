a925f67fa812d7204c7ad2d01a46e6a5d7362f56
Asilbek <asilbekdavid@gmail.com>
2023-10-20 17:47:01 +0500

    *

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^wrj=)k5irzo8zi_8h02r_pr4h7qeqzfev0$a)x$$-ej_86%&m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['16.16.209.64']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
